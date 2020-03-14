6d7ae9bfa14a1b9f5e7815a7c68c0d8a088ef0d9
nickmflorin <nickmflorin@gmail.com>
2020-03-14 17:06:41 -0400

    Include PK in Response

--- src/portfolio/app/experience/serializers.py
+++ src/portfolio/app/experience/serializers.py
@@ -3,20 +3,22 @@ from .models import Experience, Company
 
 
 class CompanySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
     name = serializers.CharField()
     city = serializers.CharField()
     state = serializers.CharField()
 
     class Meta:
         model = Company
-        fields = ('__all__', )
+        fields = ('id', 'name', 'city', 'state')
 
 
 class ExperienceSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
     company = CompanySerializer()
     start_date = serializers.DateTimeField()
     end_date = serializers.DateTimeField()
 
     class Meta:
         model = Experience
-        fields = ('__all__', )
+        fields = ('id', 'company', 'start_date', 'end_date')
