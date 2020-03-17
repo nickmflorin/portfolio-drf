600054914941d13c2026591b59b7b6cccc42fbd5
nickmflorin <nickmflorin@gmail.com>
2020-03-16 23:05:04 -0400

    Add django-cors-headers

--- src/portfolio/conf/settings/installed_apps.py
+++ src/portfolio/conf/settings/installed_apps.py
@@ -6,6 +6,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'portfolio',
     'portfolio.app.education',
     'portfolio.app.experience',
