53d0c385bbcb4f411a52792f408b214496514576
nickmflorin <nickmflorin@gmail.com>
2020-03-19 00:16:57 -0400

    Add Tests for Projects Endpoint

---
+++ tests/projects/test_views.py
@@ -0,0 +1,33 @@
+import datetime
+import pytest
+
+from portfolio.app.experience.models import Experience
+
+
+@pytest.mark.django_db
+def test_list_response_200(api_client, projects):
+    response = api_client.get('/api/v1/projects/')
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            'id': projects[0].pk,
+            'name': projects[0].name,
+            'description': projects[0].description,
+        },
+        {
+            'id': projects[1].pk,
+            'name': projects[1].name,
+            'description': projects[1].description,
+        }
+    ]
+
+
+@pytest.mark.django_db
+def test_detail_response_200(api_client, projects):
+    response = api_client.get('/api/v1/projects/%s/' % projects[0].pk)
+    assert response.status_code == 200
+    assert response.json() == {
+        'id': projects[0].pk,
+        'name': projects[0].name,
+        'description': projects[0].description,
+    }
