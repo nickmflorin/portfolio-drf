e0241a9917f054c6ffce6a3abace151396b4fb56
nickmflorin <nickmflorin@gmail.com>
2020-03-25 10:55:16 -0400

    Make State a Choice Field, Squash Migrations

--- src/portfolio/app/companies/models.py
+++ src/portfolio/app/companies/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from portfolio.app.common.constants import STATES
+
 
 def upload_to(instance, filename):
     ext = filename.split('.')[-1]
@@ -7,65 +9,11 @@ def upload_to(instance, filename):
     return f'uploads/{filename}'
 
 
-abbreviations = [
-    'AL',
-    'AK',
-    'AZ',
-    'AR',
-    'CA',
-    'CO',
-    'CT',
-    'DC',
-    'DE',
-    'FL',
-    'GA',
-    'HI',
-    'ID',
-    'IL',
-    'IN',
-    'IA',
-    'KS',
-    'KY',
-    'LA',
-    'ME',
-    'MD',
-    'MA',
-    'MI',
-    'MN',
-    'MS',
-    'MO',
-    'MT',
-    'NE',
-    'NV',
-    'NH',
-    'NJ',
-    'NM',
-    'NY',
-    'NC',
-    'ND',
-    'OH',
-    'OK',
-    'OR',
-    'PA',
-    'RI',
-    'SC',
-    'SD',
-    'TN',
-    'TX',
-    'UT',
-    'VT',
-    'VA',
-    'WA',
-    'WV',
-    'WI',
-    'WY',
-]
-
 class Company(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=512, null=True, blank=True)
     city = models.CharField(max_length=64)
-    state = models.CharField(max_length=2)
+    state = models.CharField(choices=STATES, max_length=2)
     # TODO: Migrate to django-filer
     logo = models.ImageField(upload_to=upload_to, null=True)
     date_created = models.DateTimeField(auto_now_add=True, db_index=True)
--- src/portfolio/app/schools/models.py
+++ src/portfolio/app/schools/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from portfolio.app.common.constants import STATES
+
 
 def upload_to(instance, filename):
     ext = filename.split('.')[-1]
@@ -10,7 +12,7 @@ def upload_to(instance, filename):
 class School(models.Model):
     name = models.CharField(max_length=64, unique=False)
     city = models.CharField(max_length=64, unique=False)
-    state = models.CharField(max_length=2, unique=False)
+    state = models.CharField(choices=STATES, max_length=2)
     # TODO: Migrate to django-filer
     logo = models.ImageField(upload_to=upload_to, null=True)
     description = models.CharField(max_length=512, null=True, blank=True)
