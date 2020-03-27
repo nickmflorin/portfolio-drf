3724d269ae450e102e58a5773e42425bff1cb64e
nickmflorin <nickmflorin@gmail.com>
2020-03-27 12:08:59 -0400

    Remove Unnecessary Logging

--- src/portfolio/app/skills/models.py
+++ src/portfolio/app/skills/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.db.models.signals import m2m_changed
 
 from portfolio.app.common.models import PortfolioModel
-import logging
-
-logger = logging.getLogger('portfolio')
 
 
 class Skill(PortfolioModel):
