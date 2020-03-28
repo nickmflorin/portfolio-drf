4c39c122f8e2a0a198d8efb071555c7d4b97425c
nickmflorin <nickmflorin@gmail.com>
2020-03-27 22:09:24 -0400

    Add File Field to Projects

--- src/portfolio/app/projects/models.py
+++ src/portfolio/app/projects/models.py
@@ -5,9 +5,16 @@ from django.contrib.contenttypes.models import ContentType
 from portfolio.app.common.models import PortfolioModel
 
 
+def upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{instance.name.lower().replace(' ','')}.{ext}"
+    return f'uploads/projects/{filename}'
+
+
 class Project(PortfolioModel):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=1024, null=True, blank=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
+    file = models.FileField(upload_to=upload_to, null=True)
