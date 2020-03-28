823ecb90b1e2468eec7c434ef40c0b219dc51b24
nickmflorin <nickmflorin@gmail.com>
2020-03-27 22:16:47 -0400

    Change Project File to Its Own Model

--- src/portfolio/app/projects/models.py
+++ src/portfolio/app/projects/models.py
@@ -11,10 +11,17 @@ def upload_to(instance, filename):
     return f'uploads/projects/{filename}'
 
 
+class ProjectFile(PortfolioModel):
+    file = models.FileField(upload_to=upload_to, null=True)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=1024, null=True, blank=True)
+    project = models.ForeignKey('projects.Project',
+        on_delete=models.CASCADE, related_name='files')
+
+
 class Project(PortfolioModel):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=1024, null=True, blank=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-    file = models.FileField(upload_to=upload_to, null=True)
