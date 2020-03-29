90688c8839ce69cbbc1a1a4153da75422dda00e9
nickmflorin <nickmflorin@gmail.com>
2020-03-28 20:30:57 -0400

    More Robust Squash File

---
+++ src/squash.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+declare -A apps
+apps[0]='schools'
+apps[1]='education'
+apps[2]='courses'
+apps[3]='companies'
+apps[4]='experience'
+apps[5]='projects'
+apps[6]='experience'
+apps[7]='skills'
+apps[8]='profile'
+
+python manage.py migrate
+
+delete_database(){
+  cd ../
+  find . -name "*.sqlite3" -delete
+  cd src
+}
+
+delete_migration_files(){
+  cd portfolio/app
+  find . -name "migrations" -type d -print0|xargs -0 rm -r --
+  cd ../../
+}
+
+migrate_database(){
+  # Initial Migration for Django Tables
+  python manage.py migrate
+
+  for (( i=0; i<${#apps[@]}; i++ ))
+  do
+    local application=${apps[i]}
+    echo "Performing Migrations for Application ${application}"
+    python manage.py makemigrations ${application}
+    python manage.py migrate ${application}
+  done
+}
+
+dump_data() {
+  for (( i=0; i<${#apps[@]}; i++ ))
+  do
+    local application=${apps[i]}
+    echo "Dumping Data for Application ${application}"
+    python manage.py dumpdata ${application} --output ./portfolio/fixtures/${application}.json
+  done
+}
+
+load_data() {
+  for (( i=0; i<${#apps[@]}; i++ ))
+  do
+    local application=${apps[i]}
+    echo "Loading Data for Application ${application}"
+    python manage.py loaddata ./portfolio/fixtures/${application}.json
+  done
+}
+
+manage_admin(){
+  python manage.py createsuperuser --email nickmflorin@gmail.com --username nickmflorin
+}
+
+dump_data
+delete_database
+delete_migration_files
+migrate_database
+load_data
+manage_admin
