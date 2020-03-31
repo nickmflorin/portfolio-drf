2dc3baefb7df342298f69ce3097b5047b4325154
nickmflorin <nickmflorin@gmail.com>
2020-03-30 22:00:02 -0400

    Updates to Admin CSS

--- src/portfolio/conf/settings/admin.py
+++ src/portfolio/conf/settings/admin.py
@@ -11,7 +11,7 @@ __all__ = (
 ADMIN_TOOLS_INDEX_DASHBOARD = 'portfolio.app.custom_admin.dashboard.PortfolioDashboard'
 ADMIN_TOOLS_APP_INDEX_DASHBOARD = 'portfolio.app.custom_admin.dashboard.PortfolioAppIndexDashboard'
 ADMIN_TOOLS_MENU = 'portfolio.app.custom_admin.menu.PortfolioMenu'
-ADMIN_TOOLS_THEMING_CSS = 'admin/custom_admin/css/theming.css'
+ADMIN_TOOLS_THEMING_CSS = 'admin/custom_admin/css/custom.css'
 
 
 SITE_HEADER = "Personal Portfolio Admin"
