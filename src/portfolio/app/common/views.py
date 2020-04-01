9f80195121dda53c870c3d762aa234bcc759d23b
nickmflorin <nickmflorin@gmail.com>
2020-03-31 22:23:12 -0400

    Include Request in Context

--- src/portfolio/app/common/views.py
+++ src/portfolio/app/common/views.py
@@ -24,10 +24,11 @@ class PartitionedReadOnlyViewSet(viewsets.ReadOnlyModelViewSet, metaclass=ABCMet
         pass
 
     def list(self, request):
-        serializer = self.list_serializer_class(self.queryset, many=True)
+        serializer = self.list_serializer_class(self.queryset, many=True,
+            context={'request': request})
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         obj = get_object_or_404(self.queryset, pk=pk)
-        serializer = self.detail_serializer_class(obj)
+        serializer = self.detail_serializer_class(obj, context={'request': request})
         return Response(serializer.data)
