155f711aa3be32d0e60305baa08f053bd63d4f69
nickmflorin <nickmflorin@gmail.com>
2020-04-07 13:51:07 -0400

    Remove AllowAny Default Permission

--- src/portfolio/conf/settings/restframework.py
+++ src/portfolio/conf/settings/restframework.py
@@ -2,10 +2,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
-    # DO NOT KEEP THIS IN PRODUCTION
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
     'NON_FIELD_ERRORS_KEY': '__all__',
     # 'UNAUTHENTICATED_USER': False,
 }
