7ffa2015e2e0f9e481c60cb50ab7535b3bcb7ca9
nickmflorin <nickmflorin@gmail.com>
2020-04-09 19:30:51 -0400

    Fix Bug in Signature URL of Public Images

---
+++ src/portfolio/conf/settings/aws.py
@@ -0,0 +1,10 @@
+from portfolio.conf import config
+
+AWS_ACCESS_KEY_ID = config('AWS_PORTFOLIO_GROUP_ACCESS_KEY_ID', required=True)
+AWS_SECRET_ACCESS_KEY = config('AWS_PORTFOLIO_GROUP_SECRET_ACCESS_KEY', required=True)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_STORAGE_BUCKET_NAME = 'portfolio20'
+AWS_S3_REGION_NAME = 'us-east-2'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.us-east-2.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
