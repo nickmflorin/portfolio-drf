7ed591ecc1983cc426cdd3cda95afa94a7d4a6f5
nickmflorin <nickmflorin@gmail.com>
2020-04-10 13:59:20 -0400

    Configure for Beanstalk Deployment

--- src/portfolio/conf/settings/prod.py
+++ src/portfolio/conf/settings/prod.py
@@ -8,7 +8,7 @@ LIVE = not DEBUG and not DEV
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?([\w\.-]*?)\.nickflorin\.com$',)
 
-ALLOWED_HOSTS = ['django-env.eba-5tpm7fui.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['portfolio-api.us-east-1.elasticbeanstalk.com']
 
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (  # noqa
     'rest_framework.renderers.JSONRenderer',
