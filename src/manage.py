cbdedf6efe661b4403f3951e6832604a37ceeaef
nickmflorin <nickmflorin@gmail.com>
2020-04-10 21:37:15 -0400

    Python 3.6 Compatible

--- src/manage.py
+++ src/manage.py
@@ -8,13 +8,14 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.conf.settings.local')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+        )
+    else:
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
