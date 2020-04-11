9a4e99e402a53fa9ac99343ce17aaea38cd0d75c
nickmflorin <nickmflorin@gmail.com>
2020-04-10 22:07:28 -0400

    Temporarily Suspend AWS S3

--- src/portfolio/conf/settings/prod.py
+++ src/portfolio/conf/settings/prod.py
@@ -1,4 +1,4 @@
-from .aws import *  # noqa
+# from .aws import *  # noqa
 from .base import *  # noqa
 
 DEBUG = False
@@ -8,7 +8,7 @@ LIVE = not DEBUG and not DEV
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_REGEX_WHITELIST = (r'^(https?://)?([\w\.-]*?)\.nickflorin\.com$',)
 
-ALLOWED_HOSTS = ['django-env.eba-ch6mjmhw.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['http://54.218.224.251/', 'https://54.218.224.251/']
 
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (  # noqa
     'rest_framework.renderers.JSONRenderer',
