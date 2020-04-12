aea83bb93926e87784773730a45764ed1338c0b3
nickmflorin <nickmflorin@gmail.com>
2020-04-12 14:02:02 -0400

    Boto3 Expecting AWS_STORAGE_BUCKET_NAME

--- src/portfolio/conf/settings/aws.py
+++ src/portfolio/conf/settings/aws.py
@@ -6,10 +6,11 @@ AWS_EC2_IP_ADDRESS = config('AWS_EC2_IP_ADDRESS', required=True)
 AWS_ACCESS_KEY_ID = config('AWS_PORTFOLIO_GROUP_ACCESS_KEY_ID', required=True)
 AWS_SECRET_ACCESS_KEY = config('AWS_PORTFOLIO_GROUP_SECRET_ACCESS_KEY', required=True)
 
-AWS_S3_STORAGE_NAME = config('AWS_S3_STORAGE_NAME', required=True)
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', required=True)
 AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', required=True)
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_S3_REGION_NAME, AWS_S3_STORAGE_NAME)
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (
+    AWS_S3_REGION_NAME, AWS_STORAGE_BUCKET_NAME)
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
