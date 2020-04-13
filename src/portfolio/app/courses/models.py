03cfe3cbb90885f5d2001fa998ab038b2f922dc9
nickmflorin <nickmflorin@gmail.com>
2020-04-13 09:47:25 -0400

    Make Course Description Optional

--- src/portfolio/app/courses/models.py
+++ src/portfolio/app/courses/models.py
@@ -7,6 +7,7 @@ from portfolio.app.common.models import PortfolioModel
 class Course(PortfolioModel):
     name = models.CharField(max_length=64, unique=True,
         help_text="Name of the course or class.")
-    description = RichTextField(config_name='description')
+    description = RichTextField(config_name='description',
+        null=True, blank=True)
     education = models.ForeignKey('education.Education',
         on_delete=models.CASCADE, related_name='courses')
